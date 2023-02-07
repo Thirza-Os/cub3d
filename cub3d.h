b5dd8230853076cf6e4fea47c2894fe3ee335942
Rick Brune <rbrune@f1r3s14.codam.nl>
2023-02-07 10:21:43 +0100

    rick branch

--- cub3d.h
+++ cub3d.h
@@ -6,17 +6,25 @@
 /*   By: rbrune <rbrune@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/01 10:55:06 by rbrune        #+#    #+#                 */
-/*   Updated: 2023/02/01 11:06:22 by rbrune        ########   odam.nl         */
+/*   Updated: 2023/02/01 12:49:07 by rbrune        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "./lib/MLX42/include/MLX42/MLX42.h"
-# include "./lib/libft/libft.h"
+# include "./MLX42/include/MLX42/MLX42.h"
+# include "./libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
 # include <math.h>
+
+typedef struct s_cub3d {
+	mlx_image_t	*g_img_p;
+	mlx_t		*mlx_p;
+	int			player_x;
+	int			player_y;
+	
+} t_cub3d;
 
 #endif
--- src/rickmain.c
+++ src/rickmain.c
@@ -6,21 +6,33 @@
 /*   By: rbrune <rbrune@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/01 10:54:03 by rbrune        #+#    #+#                 */
-/*   Updated: 2023/02/01 11:02:19 by rbrune        ########   odam.nl         */
+/*   Updated: 2023/02/01 11:50:17 by rbrune        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../cub3d.h"
+
+void	my_keyhook(mlx_key_data_t keydata, void *cubed)
+{
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		mlx_close_window(((t_cub3d *)cubed)->mlx_p);
+}
 
 int main (int ac, char **av)
 {
-	mlx_image_t			*g_img;
-	mlx_t				*mlx_p;
+	t_cub3d cubed;
+	int 	i = 0;
 	
-
 	(void)ac;
 	(void)av;
-	mlx_p = mlx_init(1000, 1000, "Cub3d", true);
-	g_img = mlx_new_image(mlx_p, 1000, 1000);
-	mlx_image_to_window(mlx_p, g_img, 0, 0);
+	cubed.mlx_p = mlx_init(1000, 1000, "Cub3d", true);
+	cubed.g_img_p = mlx_new_image(cubed.mlx_p, 1000, 1000);
+	mlx_image_to_window(cubed.mlx_p, cubed.g_img_p, 0, 0);
+	mlx_key_hook(cubed.mlx_p, &my_keyhook, (void *)&cubed);
+	while (i < 1000)
+	{
+		mlx_put_pixel(cubed.g_img_p, i, i, 0x666666FF);
+		i++;
+	}
+	mlx_loop(cubed.mlx_p);
 }
