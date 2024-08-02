1bc96009f127d6490d6fb5c7f9961b1525fbde44
Luuk Van Geffen <lvan-gef@f1r1s2.codam.nl>
2024-08-02 20:55:58 +0200

    We can rotate and move in the dir that we facing

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/27 21:28:19 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/08/02 19:24:16 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/08/02 20:53:03 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,63 +22,118 @@ void key_hook(void *param)
         mlx_close_window(state->mlx->mlx);
     if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_W))
     {
-        if (state->map->map[(int)state->dda->player_pos.y - 1][(int)state->dda->player_pos.x] == '0') {
-            state->dda->player_pos.y -= 1;
-            printf("Moved up to: player_pos.y = %f\n", state->dda->player_pos.y); // Debug print
-            dda(state);
+        if (state->map->map[(int)state->dda->player_pos.y + 1][(int)state->dda->player_pos.x] == '0') {
+			// double	current_pos_x;
+			double	dx;
+			double	dy;
+			// double	spacing;
+
+			// current_pos_x = state->dda->player_pos.x;
+			dx = state->dda->player_dir.x * MOVESPEED * 1;
+			// if (dx > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.x += dx;
+			dy = state->dda->player_dir.y * MOVESPEED * 1;
+			// if (dy > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.y += dy;
+			dda(state);
         }
     }
     if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_S))
     {
         if (state->map->map[(int)state->dda->player_pos.y + 1][(int)state->dda->player_pos.x] == '0') {
-            state->dda->player_pos.y += 1;
-            printf("Moved down to: player_pos.y = %f\n", state->dda->player_pos.y); // Debug print
-            dda(state);
+			// double	current_pos_x;
+			double	dx;
+			double	dy;
+			// double	spacing;
+
+			// current_pos_x = state->dda->player_pos.x;
+			dx = state->dda->player_dir.x * MOVESPEED * 1;
+			// if (dx > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.x -= dx;
+			dy = state->dda->player_dir.y * MOVESPEED * 1;
+			// if (dy > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.y -= dy;
+			dda(state);
         }
     }
     if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_A))
     {
         if (state->map->map[(int)state->dda->player_pos.y][(int)state->dda->player_pos.x - 1] == '0') {
-            state->dda->player_pos.x -= 1;
-            printf("Moved left to: player_pos.x = %f\n", state->dda->player_pos.x); // Debug print
+			// double	current_pos_x;
+			double	dx;
+			double	dy;
+			// double	spacing;
+
+			// current_pos_x = dda->player_pos.x;
+			dx = -(state->dda->player_dir.y * MOVESPEED * 1);
+			// if (dx > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.x -= dx;
+			dy = state->dda->player_dir.x * MOVESPEED * 1;
+			// if (dy > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.y -= dy;
+
             dda(state);
         }
     }
     if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_D))
     {
         if (state->map->map[(int)state->dda->player_pos.y][(int)state->dda->player_pos.x + 1] == '0') {
-            state->dda->player_pos.x += 1;
-            printf("Moved right to: player_pos.x = %f\n", state->dda->player_pos.x); // Debug print
+			// double	current_pos_x;
+			double	dx;
+			double	dy;
+			// double	spacing;
+
+			// current_pos_x = dda->player_pos.x;
+			dx = -(state->dda->player_dir.y * MOVESPEED * 1);
+			// if (dx > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.x += dx;
+			dy = state->dda->player_dir.x * MOVESPEED * 1;
+			// if (dy > 0)
+			// 	spacing = 0.32;
+			// else
+			// 	spacing = -0.32;
+			state->dda->player_pos.y += dy;
+
             dda(state);
+            // state->dda->player_pos.x += 1;
+            // printf("Moved right to: player_pos.x = %f\n", state->dda->player_pos.x); // Debug print
+            // dda(state);
         }
     }
 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_LEFT)) {
 		printf("Draai links\n");
-		// double old_dir_x = state->dda->player_dir.x;
-		// // double frameTime = (time - oldTime) / 1000.0;
-		// float rot_speed = 0.08;
-		// state->dda->player_dir.x = state->dda->player_dir.x * cos(rot_speed) - state->dda->player_dir.y * sin(-rot_speed);
-		// state->dda->player_dir.y = old_dir_x * sin(rot_speed) - state->dda->player_dir.y * cos(-rot_speed);
-		// double old_plane_x = state->dda->plane_x;
-		// state->dda->plane_x = state->dda->plane_x * cos(rot_speed) - state->dda->plane_y * sin(rot_speed);
-		// state->dda->plane_x = old_plane_x * sin(rot_speed) - state->dda->plane_y * cos(rot_speed);
-
 		t_vector old_dir;
 		double	 old_plane_x;
-		float ROTSPEED = 0.08;
 		int dir = -1;
 
 
 		old_dir.x = state->dda->player_dir.x;
-		state->dda->player_dir.x = state->dda->player_dir.x * cos(ROTSPEED * dir) - \
-			state->dda->player_dir.y * sin(ROTSPEED * dir);
-		state->dda->player_dir.y = old_dir.x * sin(ROTSPEED * dir) + \
-			state->dda->player_dir.y * cos(ROTSPEED * dir);
+		state->dda->player_dir.x = state->dda->player_dir.x * cos(ROTSPEED * dir) - state->dda->player_dir.y * sin(ROTSPEED * dir);
+		state->dda->player_dir.y = old_dir.x * sin(ROTSPEED * dir) + state->dda->player_dir.y * cos(ROTSPEED * dir);
 		old_plane_x = state->dda->plane_x;
-		state->dda->plane_x = state->dda->plane_x * cos(ROTSPEED * dir) - \
-			state->dda->plane_y * sin(ROTSPEED * dir);
-		state->dda->plane_y = old_plane_x * sin(ROTSPEED * dir) + \
-			state->dda->plane_y * cos(ROTSPEED * dir);
+		state->dda->plane_x = state->dda->plane_x * cos(ROTSPEED * dir) - state->dda->plane_y * sin(ROTSPEED * dir);
+		state->dda->plane_y = old_plane_x * sin(ROTSPEED * dir) + state->dda->plane_y * cos(ROTSPEED * dir);
 
 		dda(state);
 	}
@@ -86,7 +141,6 @@ void key_hook(void *param)
 		printf("Draai rechts\n");
 		t_vector old_dir;
 		double	 old_plane_x;
-		float ROTSPEED = 0.08;
 		int dir = 1;
 
 
@@ -104,40 +158,3 @@ void key_hook(void *param)
 		dda(state);
 	}
 }
-// void	key_hook(void *param)
-// {
-// 	t_game_state	*state;
-//
-// 	state = param;
-// 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_ESCAPE))
-// 		mlx_close_window(state->mlx->mlx);
-// 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_UP))
-// 	{
-// 		if (state->map->map[(int)state->dda->player_pos.y - 1][(int)state->dda->player_pos.x] == '0') {
-// 			state->dda->player_pos.y -= 1;
-// 			dda(state);
-// 			printf("Hello up\n");
-// 		}
-// 	}
-// 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_DOWN))
-// 	{
-// 		if (state->map->map[(int)state->dda->player_pos.y + 1][(int)state->dda->player_pos.x] == '0') {
-// 			state->dda->player_pos.y += 1;
-// 			dda(state);
-// 		}
-// 	}
-// 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_LEFT))
-// 	{
-// 		if (state->map->map[(int)state->dda->player_pos.y][(int)state->dda->player_pos.x - 1] == '0') {
-// 			state->dda->player_pos.x -= 1;
-// 			dda(state);
-// 		}
-// 	}
-// 	if (mlx_is_key_down(state->mlx->mlx, MLX_KEY_RIGHT))
-// 	{
-// 		if (state->map->map[(int)state->dda->player_pos.y][(int)state->dda->player_pos.x + 1] == '0') {
-// 			state->dda->player_pos.x += 1;
-// 			dda(state);
-// 		}
-// 	}
-// }
